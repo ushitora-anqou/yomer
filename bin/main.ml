fd83f78a67688030997ac7a53f1691c70c294b87
Ushitora Anqou <ushitora@anqou.net>
2023-09-12 22:13:14 +0900

    Use cohttp-eio to fetch a page

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,4 @@
-let () = print_endline "Hello, World!"
+let () =
+  Eio_main.run @@ fun env ->
+  let res = Cohttp_eio.Client.get env ~host:"www.example.org" "/" in
+  print_string @@ Cohttp_eio.Client.read_fixed res
