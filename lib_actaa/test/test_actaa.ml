d7a1b9b7150175b854885444baa534ae11357c5b
Ushitora Anqou <ushitora@anqou.net>
2023-10-20 21:06:39 +0900

    Add smoke test for Actaa.Gen_server

--- lib_actaa/test/test_actaa.ml
+++ lib_actaa/test/test_actaa.ml
@@ -119,6 +119,58 @@ let test_actaa_supervisor () =
   spawn env ~sw () (new p7);
   ()
 
+module Gen_server_case1 = struct
+  type init_arg = unit
+  type call_msg = [ `Set of int | `Get ]
+  type call_reply = [ `Set of int | `Get of int ]
+  type cast_msg = [ `Set of int ]
+  type state = int
+  type basic_msg = (call_msg, call_reply, cast_msg) Gen_server.basic_msg
+  type msg = [ basic_msg | `Info of int ]
+
+  class t =
+    object
+      inherit [init_arg, msg, state] Gen_server.t
+      method private init _env ~sw:_ () = 0
+
+      method! private handle_call _env ~sw:_ x =
+        function `Set y -> `Reply (`Set x, y) | `Get -> `Reply (`Get x, x)
+
+      method! private handle_cast _env ~sw:_ _ = function `Set x -> `NoReply x
+
+      method! private handle_info _env ~sw:_ _ =
+        function #basic_msg -> assert false | `Info x -> `NoReply x
+    end
+
+  let get (t : t) =
+    match Gen_server.call t `Get with `Get x -> x | _ -> assert false
+
+  let call_set (t : t) x =
+    match Gen_server.call t (`Set x) with `Set x -> x | _ -> assert false
+
+  let cast_set (t : t) x = Gen_server.cast t (`Set x)
+
+  let expect_eventually (t : t) x =
+    let rec loop () = if get t = x then () else loop () in
+    loop ();
+    true
+
+  let test () =
+    Eio_main.run @@ fun env ->
+    Eio.Switch.run @@ fun sw ->
+    let t = new t in
+    t |> spawn env ~sw ();
+    assert (get t = 0);
+    assert (call_set t 1 = 0);
+    assert (get t = 1);
+    cast_set t 2;
+    assert (expect_eventually t 2);
+    Process.send t (`Info 3);
+    assert (expect_eventually t 3);
+    Gen_server.stop t;
+    ()
+end
+
 let () =
   Alcotest.run "actaa"
     [
@@ -128,4 +180,5 @@ let () =
           Alcotest.test_case "registry" `Quick test_actaa_registry;
           Alcotest.test_case "supervisor" `Quick test_actaa_supervisor;
         ] );
+      ("gen_server", [ Alcotest.test_case "case1" `Quick Gen_server_case1.test ]);
     ]
