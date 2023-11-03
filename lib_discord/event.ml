cb341520a5e807b8bc0af3189d1712cc2e0775bd
Ushitora Anqou <ushitora@anqou.net>
2023-11-03 21:32:28 +0900

    Add Dispatch MESSAGE_UPDATE event

--- lib_discord/event.ml
+++ lib_discord/event.ml
@@ -45,10 +45,14 @@ type dispatch_channel_topic_update = { guild_id : string }
 type dispatch_dummy = { dummy : int option [@yojson.option] }
 [@@yojson.allow_extra_fields] [@@deriving yojson, show]
 
+type dispatch_message_update = { id : string; channel_id : string }
+[@@yojson.allow_extra_fields] [@@deriving yojson, show, make]
+
 type dispatch =
   | CHANNEL_TOPIC_UPDATE of dispatch_channel_topic_update
   | GUILD_CREATE of dispatch_guild_create
   | MESSAGE_CREATE of Object.message
+  | MESSAGE_UPDATE of dispatch_message_update
   | READY of dispatch_ready
   | RESUMED of dispatch_dummy
   | VOICE_CHANNEL_STATUS_UPDATE of dispatch_voice_channel_status_update
